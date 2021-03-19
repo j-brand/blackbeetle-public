3accfa493c7fe15e12cbbe25d635091911ea824c
johbra90 <johbra90@bitbucket.org>
2021-03-19 15:43:05 +0100

    env deleted

--- src/common/envionment.ts
+++
@@ -1,7 +0,0 @@
-const backgendURL = "http://localhost:8001";
-
-export const environment = {
-  publicUrl: backgendURL,
-  apiUrl: `${backgendURL}/api`,
-  googleApiKey: "AIzaSyAMCD7m7-Fyu6RZMkN76BzFdTDFQaRlg5E",
-};
