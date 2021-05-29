980e40531478a7c5e18d58a5236701671bf7024d
johbra90 <johbra90@bitbucket.org>
2021-05-29 22:52:36 +0200

    menu state global

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,38 @@
 import Vuex from "vuex";
 
+import { ActionTypes } from "./action.type";
+import { MutationTypes } from "./mutation.type";
+
 import { album } from "./album.module";
 import { story } from "./story.module";
 
+const state = {
+  state: {
+    menuOpen: false,
+  },
+  mutations: {
+    [MutationTypes.SET_MENU_STATE](state: any, open: boolean) {
+      state.menuOpen = open;
+    },
+    [MutationTypes.TOGGLE_MENU](state: any) {
+      state.menuOpen = !state.menuOpen;
+    },
+  },
+  actions: {
+    [ActionTypes.SET_MENU_STATE](context: any, menuOpen: boolean) {
+      context.commit(MutationTypes.SET_MENU_STATE, menuOpen);
+    },
+    [ActionTypes.TOGGLE_MENU](context: any) {
+      context.commit(MutationTypes.TOGGLE_MENU);
+    },
+  },
+  getters: {
+    menuOpen: (state) => {
+      return state.menuOpen;
+    },
+  },
+};
+
 export default new Vuex.Store({
-  modules: { album, story },
+  modules: { state, album, story },
 });
