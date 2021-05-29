73abb4dfe11f535b641e7260cea9d4bdc71acc47
johbra90 <johbra90@bitbucket.org>
2021-05-30 00:06:36 +0200

    conent and formatting change

--- src/views/Imprint.vue
+++ src/views/Imprint.vue
@@ -1,24 +1,42 @@
 <template>
-    <div class="container mx-auto max-w-screen-md min-h-full pt-40 pb-24 px-5 md:px-0">
-      <h1 class="text-3xl font-medium mb-5">Impressum</h1>
+  <div class="container mx-auto max-w-screen-md min-h-full pt-40 pb-24 px-5 lg:px-0">
+    <h1 class="text-3xl font-medium mb-5">Impressum</h1>
 
-      <p>
-        Diese Seite wird betrieben von Johannes Brand<br />
-      </p>
+    <p>Diese Seite wird betrieben von Johannes Brand<br /></p>
 
-      <h2 class="mb-1 mt-5 text-lg font-medium">Kontakt</h2>
-      <p>E-Mail: <a class="text-bb-light-red underline" href="mailto:postbox@blackbeetle.de">postbox@blackbeetle.de</a></p>
+    <h2 class="mb-1 mt-5 text-lg font-medium">Kontakt</h2>
+    <p>E-Mail: <a class="text-bb-light-red underline" href="mailto:postbox@blackbeetle.de">postbox@blackbeetle.de</a></p>
 
-      <h3 class="mb-1 mt-5 text-lg font-medium">Urheberrecht</h3>
-      <p>
-        Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielf&auml;ltigung, Bearbeitung, Verbreitung und jede Art der
-        Verwertung au&szlig;erhalb der Grenzen des Urheberrechtes bed&uuml;rfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur f&uuml;r
-        den privaten, nicht kommerziellen Gebrauch gestattet.
-      </p>
-      <p>
-        Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie
-        trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend
-        entfernen.
-      </p>
-    </div>
+    <h3 class="mb-1 mt-5 text-lg font-medium">Haftung für Inhalte</h3>
+    <p>
+      Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht
+      verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+    </p>
+    <p>
+      Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt
+      der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+    </p>
+
+    <h3 class="mb-1 mt-5 text-lg font-medium">Haftung für Links</h3>
+    <p>
+      Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte
+      der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft.
+      Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+    </p>
+    <p>
+      Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir
+      derartige Links umgehend entfernen.
+    </p>
+
+    <h3 class="mb-1 mt-5 text-lg font-medium">Urheberrecht</h3>
+    <p>
+      Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung
+      außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht
+      kommerziellen Gebrauch gestattet.
+    </p>
+    <p>
+      Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie
+      trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+    </p>
+  </div>
 </template>
